740edba13738d931af17bbcffb491c1e690ac959
YuichiOta <rb19780519@gmail.com>
2025-03-16 14:48:36 +0900

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="w-full h-screen grid place-items-center">
+      <div className="
+        w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh]
+        max-w-[90vw] max-h-[90vh] border border-gray-300
+        rounded-2xl resize">
+      </div>
+    </div>
+  )
+}
